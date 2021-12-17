940f3c85a9e4007284475ebd279d4609308fdf4b
kzarius <kaeomclark@gmail.com>
2021-12-17 15:47:27 +0800

    Removed Node.js, simplified, using Flask

--- python/sensors.py
+++ python/sensors.py
@@ -1,31 +1,37 @@
-import redis
-import time
-import datetime
-import sched
-import Adafruit_DHT
+import flask
+from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+#import Adafruit_DHT
 
-REPEATER = sched.scheduler(time.time, time.sleep)
-CACHE = redis.Redis(host='redis', port=6379)
+SCHEDULER = BackgroundScheduler()
+APP = flask.Flask(__name__)
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 # The GPIO pin that links to the data pin on your DHT sensor.
 DHT_PIN = 3
 
-def getReadings(repeater_obj):
+timestamp = None
+humidity = None
+temperature = None
+
+def getReadings():
+    global humidity, temperature, timestamp
     # Gather readings from the sensor.
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-
-    # Only write to redis if the sensor returns readings.
-    if humidity is not None and temperature is not None:
-        CACHE.update({
-            "timestamp":str(datetime.datetime.now()).split('.')[0],
-            "temperature":temperature,
-            "humidity":humidity
-        })
-
-    # Schedule another refresh in 5 seconds.
-    REPEATER.enter(5,1,getReadings,(repeater_obj,))
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 # Schedule the first refresh in 5 seconds.
-REPEATER.enter(5,1,getReadings,(REPEATER,))
-REPEATER.run()
+SCHEDULER.add_job(func=getReadings, trigger="interval", seconds=5)
+SCHEDULER.start()
+
+@APP.route('/')
+def hello():
+    global humidity, temperature, timestamp
+    return flask.jsonify(
+        timestamp=timestamp,
+        temperature=temperature,
+        humidity=humidity
+    )
+
+if __name__ == "__main__":
+    APP.run(host="0.0.0.0", port='4000', debug=True)
